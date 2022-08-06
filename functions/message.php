23c62846e1247bca8d758fc527a8550f38ba0a0d
Vanessa Bonfim <vanesssabonfim@gmail.com>
2022-08-06 14:33:40 +0100

    Create a function to show a message kept inside the session

--- functions/message.php
+++ functions/message.php
@@ -17,3 +17,27 @@ function set_flash_message($message = '') {
     $_SESSION['flash_message_timestamp'] = $timestampNowPlus1Sec; //Gravar o nosso tempo dentro de uma chave na super global $_SESSION
 
 }
+
+/**
+ * 
+ * Esta função exibe uma mensagem mantida 1 segundo em uma sesssão no servidor, a função 'set_flash_message' deverá ser
+ * chamada antes.
+ * 
+ */
+
+function get_flash_message() {
+
+
+
+    if (empty($_SESSION['flash_message'])) {  // A condição testa se existe algum valor na chave 'flash_message' da super global $_SESSION. Se não existir valores, retorna null (retorna vazio).
+        return null;
+    }
+
+    $flashMessage = $_SESSION['flash_message']; // Atribuir o valor da chave 'flash_message' que está na super global $_SESSION para dentro da variável $flashMessage.
+
+    $timestampNow = strtotime('now'); // Atribuir o conjunto de números que representa o TIMESTAMP deste exacto momento dentro da variável $timestampNow.
+
+    $timestampFlashMessage = $_SESSION['flash_message_timestamp']; // Atribuir o valor da chave 'flash_message_timestamp' que está na super global $_SESSION para dentro da variável $timestampFlashMessage.
+
+    
+}
