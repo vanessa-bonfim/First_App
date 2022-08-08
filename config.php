d8ce921ca410d029c15c2ff758306585edc42122
Vanessa Bonfim <vanesssabonfim@gmail.com>
2022-08-08 13:09:49 +0100

    Create 3 constants from login, password and page title.

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('USER_LOGIN','admin'); //Constate para login
+define('USER_PASSWORD', 123456); //Constante para pass
+define('PAGE_TITLE','Exercício'); // contante para título da página
