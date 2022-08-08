a49651a85d1eb73b55b60fa269222aab898c241d
Vanessa Bonfim <vanesssabonfim@gmail.com>
2022-08-08 14:21:46 +0100

    Start the sessions, import config, url and message php files.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+session_start(); //
+
+require_once('config.php'); //importa as informações do ficheiro config.php
+
+require_once('functions/url.php'); //importa as funções do ficheiro url.php
+require_once('functions/message.php'); //importa as funções do ficheiro message.php
