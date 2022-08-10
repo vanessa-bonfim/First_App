625a587de5589534a342a79d74177e0e365906f8
Vanessa Bonfim <vanesssabonfim@gmail.com>
2022-08-10 21:07:19 +0100

    Create a condition to verificate if the login and password constants are equal that the login and password variables. Correct the function' parameters.

--- controllers/authenticate.php
+++ controllers/authenticate.php
@@ -6,11 +6,11 @@
 
 if (empty($_POST['username']) || empty($_POST['password'])) { // função empty para verificar se está vazio.
 
-    //A função set_flash_message() dispara uma mensagem com a indicação abaixo por 1 sec. (Some ao fazer refresh a página)
+    //A função set_flash_message() (função criada em message.php) dispara uma mensagem com a indicação abaixo por 1 sec. (Some ao fazer refresh a página)
     set_flash_message('Todos os campos são de preenchimento obrigatório!');
 
-    //A função url_redirect() redireciona para a página de login.
-    url_redirect($values = ['route' => 'login']);
+    //A função url_redirect() (função criada em url.php) redireciona para a página de login.
+    url_redirect(['route' => 'login']);
 
 }
 
@@ -21,4 +21,28 @@ if (empty($_POST['username']) || empty($_POST['password'])) { // função empty 
 
 $login = $_POST['username']; 
 $password = $_POST['password'];
+
+/* 
+*
+* Essa condição verifica se os valores guardados nas variáveis
+* são iguais ('==') aos que estão definidas nas contantes de login e password.
+* Na chamada das funções só é preciso colocar as informações que deseja, 
+* pois ao criarmos as funções deixamos o parametro a receber vazio por padrão.
+*/
+
+if ($login == USER_LOGIN && $password == USER_PASSWORD) { // condição é se as variáveis são iguais as constantes(config.php)
+
+    $_SESSION['is_authenticate'] = true; // ??
+
+    set_flash_message('Utilizador autenticado com sucesso!'); //Mostra essa mensagem por 1 sec. (função criada em message.php)
+
+    url_redirect(['route' => 'dashboard']); // Redireciona para página dashboard. (função criada em url.php)
+
+} else {
+    
+    set_flash_message('Utilizador ou senha incorreta, tente novamente!');// Mostra essa mensagem por 1 sec. (função criada em message.php)
+
+    url_redirect(['route' => 'login']); //Redireciona para página login. (função criada em url.php)
+}
+
 
